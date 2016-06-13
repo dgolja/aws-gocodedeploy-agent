1ab1492affacd8cb45ad618a65fa18ba408a2533
Dejan Golja <dejan.golja@fairfaxmedia.com.au>
2016-06-13 22:18:47 +1000

    TODO: implement restart

--- cmd/restart.go
+++ cmd/restart.go
@@ -28,21 +28,10 @@ var restartCmd = &cobra.Command{
   restart - restart the AWS CodeDeploy agent`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// TODO: Work your own magic here
-		fmt.Println("restart called")
+		fmt.Println("restart called - implement")
 	},
 }
 
 func init() {
 	RootCmd.AddCommand(restartCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// restartCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// restartCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-
 }
